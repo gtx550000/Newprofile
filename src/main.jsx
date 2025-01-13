89041143bc6c05e44716a2ec572b4f0db528305a
gtx550000 <114909099+gtx550000@users.noreply.github.com>
2025-01-13 17:03:04 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter basename="/Newprofile">
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
